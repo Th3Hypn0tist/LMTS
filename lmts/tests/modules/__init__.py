21a4e3b2b5ae7a0b251a3569ff7047d96d97dae5
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-09-13 11:35:05 +0300

    Export free prompt consistency test module

--- lmts/tests/modules/__init__.py
+++ lmts/tests/modules/__init__.py
@@ -1,4 +1,10 @@
+from .free_prompt import FreePromptConsistencyTest, FreePromptTest
 from .text_generation import TextGenerationTest
 from .workspace_multifile import WorkspaceMultiFileTest
 
-__all__ = ["TextGenerationTest", "WorkspaceMultiFileTest"]
+__all__ = [
+    "FreePromptConsistencyTest",
+    "FreePromptTest",
+    "TextGenerationTest",
+    "WorkspaceMultiFileTest",
+]

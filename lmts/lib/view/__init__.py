e8066f665beaf266e001a57e4c97b587c9bfdd3d
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-09-13 21:07:57 +0300

    Export view registries

--- lmts/lib/view/__init__.py
+++ lmts/lib/view/__init__.py
@@ -3,6 +3,19 @@
 from .curses_host import CursesViewHost
 from .model import ViewFrame, ViewItem
 from .path_dialog import choose_directory
+from .shortcut_registry import ShortcutDefinition, ShortcutMatch, ShortcutRegistry
 from .split_host import SplitCursesViewHost
+from .tab_registry import TabDefinition, TabRegistry
 
-__all__ = ["CursesViewHost", "SplitCursesViewHost", "ViewFrame", "ViewItem", "choose_directory"]
+__all__ = [
+    "CursesViewHost",
+    "SplitCursesViewHost",
+    "ViewFrame",
+    "ViewItem",
+    "choose_directory",
+    "ShortcutDefinition",
+    "ShortcutMatch",
+    "ShortcutRegistry",
+    "TabDefinition",
+    "TabRegistry",
+]

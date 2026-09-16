2d7d96359ffd3d1b92870cf7c0a15849ee950204
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-09-16 19:50:26 +0300

    Attach settings service to TUI state

--- lmts/view/tui_state.py
+++ lmts/view/tui_state.py
@@ -5,6 +5,7 @@ from typing import Any
 
 from lmts.core.settings import LMTSSettings
 from lmts.lib.view import UIEventBus
+from lmts.services.settings import SettingsService
 
 from .controller import LMTSViewController
 from .cw_bench_page import CWBenchPage
@@ -17,8 +18,9 @@ class TUIState:
     projector: LMTSViewProjector
     cw_bench_page: CWBenchPage
     settings: LMTSSettings
+    settings_service: SettingsService
     dvs_service_state: Any
-    shortcut_overrides: dict[str, str]
+    shortcut_overrides: dict[str, tuple[str, ...]]
     shortcuts: Any
     events: UIEventBus
     active_tab: str = 'profile'

d14b6d1a6fa7daf2813de6379257b4f80c28f871
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-09-16 17:57:48 +0300

    Carry shared UI event bus in TUI state

--- lmts/view/tui_state.py
+++ lmts/view/tui_state.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 from typing import Any
 
 from lmts.core.settings import LMTSSettings
+from lmts.lib.view import UIEventBus
 
 from .controller import LMTSViewController
 from .cw_bench_page import CWBenchPage
@@ -19,5 +20,6 @@ class TUIState:
     dvs_service_state: Any
     shortcut_overrides: dict[str, str]
     shortcuts: Any
+    events: UIEventBus
     active_tab: str = 'profile'
     profile_console: list[str] = field(default_factory=list)
